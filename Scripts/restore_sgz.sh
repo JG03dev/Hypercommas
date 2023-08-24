afde3ff425ddabbd7d0db32db38d3a38003b83b2
JG03dev <jan.gra.ser.001@gmail.com>
2023-08-24 10:11:38 +0200

    Changed grub theme to minegrub

--- Scripts/restore_sgz.sh
+++ Scripts/restore_sgz.sh
@@ -47,7 +47,7 @@ if pkg_installed grub
 
         sudo sed -i "/^GRUB_DEFAULT=/c\GRUB_DEFAULT=saved
         /^GRUB_GFXMODE=/c\GRUB_GFXMODE=1280x1024x32,auto
-        /^#GRUB_THEME=/c\GRUB_THEME=\"/usr/share/grub/themes/pochita/theme.txt\"
+        /^#GRUB_THEME=/c\GRUB_THEME=\"/usr/share/grub/themes/minegrub/theme.txt\"
         /^#GRUB_SAVEDEFAULT=true/c\GRUB_SAVEDEFAULT=true" /etc/default/grub
 
         sudo cp /boot/grub/grub.cfg /boot/grub/grub.t2.bkp
